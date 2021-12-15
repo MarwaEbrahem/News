da462a29a0bde6721cc60d9b7e267d6302e0e348
MarwaEbrahem <marwa15shanab@gmail.com>
2021-12-15 03:24:04 -0600

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by marwa on 12/15/21.
+//  Copyright © 2021 marwa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
